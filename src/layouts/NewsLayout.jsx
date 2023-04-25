22423cb3d0312f5b99651c7d88ff2652229e0377
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-04-25 11:03:48 +0600

    news dragon client added

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -5,20 +5,15 @@ import { Col, Container, Row } from 'react-bootstrap';
 import LeftNav from '../pages/Shared/LeftNav/LeftNav';
 import RightNav from '../pages/Shared/RightNav/RightNav';
 import { Outlet } from 'react-router-dom';
-import NavigationBar from '../pages/Shared/NavigationBar/NavigationBar';
 
-const Main = () => {
+const NewsLayout = () => {
     return (
         <div>
             <Header></Header>
-            <NavigationBar></NavigationBar>
             <Container>
-             
+
                 <Row>
-                    <Col lg={3} sm>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col lg={6} sm>
+                    <Col lg={9} sm>
                         <Outlet></Outlet>
                     </Col>
                     <Col lg={3} sm>
@@ -31,4 +26,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default NewsLayout;
